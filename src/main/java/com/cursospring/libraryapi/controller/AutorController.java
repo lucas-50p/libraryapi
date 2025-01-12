7a2a03516d3eaa3a3f75a3128ce281f29443ba8c
Lucas Bueno <lucas-50p@hotmail.com>
2025-01-12 11:54:42 -0300

    79. Iniciando a codificação da API

---
+++ src/main/java/com/cursospring/libraryapi/controller/AutorController.java
@@ -0,0 +1,16 @@
+package com.cursospring.libraryapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("autores")
+// http://localhost:8080/autores
+public class AutorController {
+
+    // GET
+
+    // POST
+    // DELETE
+    // PUT
+}
